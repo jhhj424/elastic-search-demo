e363650164eaa2d05e35b06c4ae2f486ed021ea3
zito-s <jhhj424@naver.com>
2023-01-08 21:22:10 +0900

    chore: elastic search config

---
+++ src/main/java/com/zito/elasticsearchdemo/config/ElasticSearchConfig.java
@@ -0,0 +1,39 @@
+package com.zito.elasticsearchdemo.config;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@EnableElasticsearchRepositories
+@Configuration
+@RequiredArgsConstructor
+public class ElasticSearchConfig extends ElasticsearchConfiguration {
+
+    private final ElasticSearchProperties elasticSearchProperties;
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo(elasticSearchProperties.url)
+                .withConnectTimeout(Duration.ofSeconds(5))
+                .withSocketTimeout(Duration.ofSeconds(3))
+                .build();
+    }
+
+    @Component
+    @ConfigurationProperties(prefix = "elasticsearch")
+    @Getter
+    @Setter
+    public static class ElasticSearchProperties {
+        private String url;
+    }
+
+}
